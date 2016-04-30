9c2280aa553e8c5adc69ca61c50106c1ad39dd6b
kiryl <kirylstasevich@gmail.com>
2016-04-30 20:08:31 +0300

    refactoring of course model

--- models/course.js
+++ models/course.js
@@ -4,11 +4,14 @@ module.exports = function(sequelize, DataTypes) {
   const course = sequelize.define('course', {
     courseId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, field: 'COURSE_ID' },
     courseType: { type: DataTypes.STRING, field: 'COURSE_TYPE' },
+    courseTypeTransl: { type: DataTypes.STRING, field: 'COURSE_TYPE_TRANSL'},
     password: { type: DataTypes.STRING, field: 'PASSWORD' },
     percentForSuccess: { type: DataTypes.INTEGER, field: 'PERCENT_FOR_SUCCESS' },
     timeForExecuting: { type: DataTypes.INTEGER, field: 'TIME_FOR_EXECUTING' },
     maxNumberOfAttemps: { type: DataTypes.INTEGER, field: 'MAX_NUMBER_OF_ATTEMPS' },
-    numberOfQuestions: { type: DataTypes.INTEGER, field: 'NUMBER_OF_QUESTIONS' }
+    minCostToLadder: { type: DataTypes.INTEGER, field: 'MIN_COST_TO_LADDER' },
+    permitToUseMinRule: { type: DataTypes.INTEGER, field: 'PERMIT_TO_USE_MIN_RULE' },
+    permitToCleverCount: { type: DataTypes.INTEGER, field: 'PERMIT_TO_CLEVER_COUNT' }
   }, {
     freezeTableName: true,
     classMethods: {
